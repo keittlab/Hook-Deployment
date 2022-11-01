2900528aba2a98fc95dd4f71b557fc65a7ff3e2b
anniekep <45701166+anniekep@users.noreply.github.com>
2022-11-01 16:19:41 -0500

    Added sensor data transfer file

---
+++ transfersensorfiles.bash
@@ -0,0 +1,21 @@
+#!/bin/bash
+SENSORFILES_DIR="/DATA/environmental"
+SOUNDFILES_DIR="/home/pi/upstream/sound"
+TIMESTAMP=`(date +%Y%m%d)`
+echo "Getting the most recent files from $SENSORFILES_DIR"
+ls -Art $SENSORFILES_DIR|grep csv|tail -n 2 > $SENSORFILES_DIR/../logs/sensor-xfer-temp.log-$TIMESTAMP
+
+if [[ $(cat $SENSORFILES_DIR/../logs/sensor-xfer-temp.log-$TIMESTAMP| wc -l) -eq 0 ]]; then
+        echo " "
+        echo "NOTE: There are no csv data files to clean up in $SENSORFILES_DIR"
+        echo " "
+        exit 0
+fi
+
+echo "Sending files."
+
+cat $SENSORFILES_DIR/../logs/sensor-xfer-temp.log-$TIMESTAMP | while read line; do
+        if [[ `(/usr/bin/python3 $SOUNDFILES_DIR/../stengl-minio-tests/stengl-minio-md5check-cleanoutpu>              echo "File $SENSORFILES_DIR/$line already sync'd"
+        else
+               /usr/bin/python3 $SOUNDFILES_DIR/../stengl-minio-tests/sendtocorral-minio.py $SENSORFILE>        fi;
+done
