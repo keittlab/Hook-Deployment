d3e98138c6021332f3586bdec340a218a67786f8
anniekep <45701166+anniekep@users.noreply.github.com>
2022-11-15 15:12:11 -0600

    Updated Path

--- cleanup-all-transferedsensorfiles.bash
+++ cleanup-all-transferedsensorfiles.bash
@@ -1,5 +1,6 @@
 #!/bin/bash
 SENSORFILES_DIR="/DATA/environmental"
+SOUNDFILES_DIR="/home/pi/upstream/sound"
 TIMESTAMP=`(date +%Y%m%d)`
 echo "Getting the most recent sensorfiles from $SENSORFILES_DIR"
 ls -Art $SENSORFILES_DIR|grep csv > $SENSORFILES_DIR/../logs/environmental-xfer-temp.log-$TIMESTAMP
@@ -18,6 +19,6 @@ cat $SENSORFILES_DIR/../logs/environmental-xfer-temp.log-$TIMESTAMP | while read
 	if [[ `(/usr/bin/python3 $SOUNDFILES_DIR/../stengl-minio-tests/stengl-minio-md5check-cleanoutput.py $SENSORFILES_DIR/$line)` == true ]]; then
 	      echo "File $SENSORFILES_DIR/$line already sync'd"
         else 
-               /usr/bin/python3 $SENSORFILES_DIR/../stengl-minio-tests/sendtocorral-minio.py $SENSORFILES_DIR/$line
+               /usr/bin/python3 $SOUNDFILES_DIR/../stengl-minio-tests/sendtocorral-minio.py $SENSORFILES_DIR/$line
 	fi;
 done
